a2671b726a3ae971996a1cd1483199272b37e982
harikishoremishra <122509569+harikishoremishra@users.noreply.github.com>
2023-07-11 11:26:05 +0530

    Add files via upload

---
+++ arrays passing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10];
+	cout<<"values for comparision:"<<endl;
+	
+	cout<<"The values of a is"<<a<<endl;
+	cout<<"The values of address a[0] is"<<&a[0]<<endl;
+	cout<<"The values of a[1] is"<<&a[1]<<endl;
+}
